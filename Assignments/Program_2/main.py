4c1de4b9dcce5ab7210ee1ae1e945ab776f30ad7
13oT <A.alathel93@gmail.com>
2017-06-18 02:36:11 -0500

    addin stuff

--- Assignments/Program_2/main.py
+++ Assignments/Program_2/main.py
@@ -6,10 +6,10 @@ import pygame
 import random
 import json
 import os
-import pprint as pp
 
-DIRPATH = os.path.abspath (
-    os.path.join ( os.path.dirname ( __file__ ), '..', '..', '..', '4553-Spatial-DS', 'Resources' ) )
+DIRPATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', '..', '..', '4553-Spatial-DS', 'Resources'))
+
 
 def clean_area(screen, origin, width, height, color):
     """
@@ -17,19 +17,21 @@ def clean_area(screen, origin, width, height, color):
     Could be used to erase a small area, or the entire screen.
     """
     ox, oy = origin
-    points = [(ox, oy), (ox + width, oy), (ox + width, oy + height), (ox, oy + height), (ox, oy)]
-    pygame.draw.polygon ( screen, color, points, 0 )
+    points = [(ox, oy), (ox + width, oy), (ox + width,
+                                           oy + height), (ox, oy + height), (ox, oy)]
+    pygame.draw.polygon(screen, color, points, 0)
 
-class Colors ( object ):
+
+class Colors (object):
     """
     Opens a json file of web colors.
     """
 
     def __init__(self, file_name=DIRPATH + '/Json_Files/colors.json'):
-        with open ( file_name, 'r' ) as content_file:
-            content = content_file.read ()
+        with open(file_name, 'r') as content_file:
+            content = content_file.read()
 
-        self.content = json.loads ( content )
+        self.content = json.loads(content)
 
     def get_random_color(self):
         """
@@ -75,7 +77,7 @@ class Colors ( object ):
         return self.get_rgb(color_name)
 
 
-def scaled(crimes,width,height):
+def scaled(crimes, width, height):
     """
 scale the x, y coordianets to the screen
     """
@@ -86,15 +88,18 @@ scale the x, y coordianets to the screen
     max_x = max(points)[0]
     min_y = min(points)[1]
     max_y = max(points)[1]
+
     for key in crimes.keys():
         lst = []
         for i in crimes[key]['location']:
             x = int(width * ((i[0] - min_x) / ((max_x - min_x))))
             y = int(height * ((i[1] - min_y) / ((max_y - min_y))))
-            y=-y+920
+            y = -y + 920
             lst.append(tuple((x, y)))
         crimes[key]['location'] = lst
 
+
+crimes_area = {}
 
 
 def read_crimes():
@@ -104,18 +109,20 @@ read in the five files and store them in a dict (crimes)
     crimes = {}
     color = Colors()
     got_keys = False
-    crime_locations = ['bronx', 'brooklyn', 'manhattan', 'queens', 'staten_island']
-    crimes['random'] = {'location': [],'color': color.get_random_color(), 'mbrs': []}
+    crime_locations = ['bronx', 'brooklyn',
+                       'manhattan', 'queens', 'staten_island']
+    crimes['random'] = {'location': [],
+                        'color': color.get_random_color(), 'mbrs': []}
 
     for crime_place in crime_locations:
+        crimes_area[crime_place] = {'location': [], 'color': None}
         with open(DIRPATH + '/NYPD_CrimeData/filtered_crimes_' + crime_place + '.csv') as f:
             for line in f:
                 line = ''.join(x if i % 2 == 0 else x.replace(',', ':')
-                            for i, x in enumerate(line.split('"')))
+                               for i, x in enumerate(line.split('"')))
                 line = line.strip().split(',')
                 if not got_keys:
                     keys = line
-                    print(keys)
                     got_keys = True
                     continue
 
@@ -123,12 +130,16 @@ read in the five files and store them in a dict (crimes)
                     if e == '':
                         line.remove(e)
                 try:
-                    temp=int(line[7])
+
+                    temp = int(line[7])
                     if line[9] not in crimes:
                         crimes[line[9]] = {
                             'location': [], 'color': color.get_random_color(), 'mbrs': []}
                     try:
+
                         crimes[line[9]]['location'].append(
+                            tuple((float(line[-5]), float(line[-4]))))
+                        crimes_area[crime_place]['location'].append(
                             tuple((float(line[-5]), float(line[-4]))))
                     except:
                         continue
@@ -139,10 +150,14 @@ read in the five files and store them in a dict (crimes)
                     try:
                         crimes[line[7]]['location'].append(
                             tuple((float(line[-5]), float(line[-4]))))
+                        crimes_area[crime_place]['location'].append(
+                            tuple((float(line[-5]), float(line[-4]))))
                     except:
                         continue
                 try:
                     crimes['random']['location'].append(
+                        tuple((float(line[-5]), float(line[-4]))))
+                    crimes_area[crime_place]['location'].append(
                         tuple((float(line[-5]), float(line[-4]))))
                 except:
                     continue
@@ -150,22 +165,25 @@ read in the five files and store them in a dict (crimes)
 
 
 crimes = read_crimes()
-scaled(crimes,950,515)
-text=open(os.path.abspath(os.path.join(os.path.dirname ( __file__ ),'..','..','..','random','WHATEVER')),'w')
-pp.pprint(crimes['random'],text)
-text.close()
+scaled(crimes, 950, 515)
+scaled(crimes_area, 950, 515)
+crimes_area['manhattan']['color'] = (194, 35, 38)
+crimes_area['queens']['color'] = (243, 115, 56)
+crimes_area['staten_island']['color'] = (253, 182, 50)
+crimes_area['brooklyn']['color'] = (128, 22, 56)
+crimes_area['bronx']['color'] = (2, 120, 120)
 background_colour = (255, 255, 255)
 black = (0, 0, 0)
-(width, height) = (1024, 900)
+(width, height) = (1000, 1000)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('New York Crimes')
 screen.fill(background_colour)
 pygame.display.flip()
 running = True
 while running:
-    for key in crimes.keys():
-        for p in crimes[key]['location']:
-            pygame.draw.circle(screen, crimes[key]['color'], p, 2, 0)
+    for key in crimes_area.keys():
+        for p in crimes_area[key]['location']:
+            pygame.draw.circle(screen, crimes_area[key]['color'], p, 2, 0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
